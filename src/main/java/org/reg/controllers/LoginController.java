5845bd2cd55ee9897b9b2bcefad8ba89a843dd09
cristiberzescu <berzescu.c31@gmail.com>
2022-05-14 21:25:51 +0300

    [AA-24] create register structure

--- src/main/java/org/reg/controllers/LoginController.java
+++ src/main/java/org/reg/controllers/LoginController.java
@@ -1,7 +1,12 @@
 package org.reg.controllers;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 import java.io.IOException;
 
@@ -21,6 +26,12 @@ public class LoginController {
 
     }
     public void goToRegistration(javafx.event.ActionEvent register) throws IOException {
-
+        FXMLLoader Loader = new FXMLLoader();
+        Loader.setLocation(getClass().getClassLoader().getResource("register.fxml"));
+        Parent viewRegister = Loader.load();
+        Scene RegisterScene = new Scene(viewRegister, 590, 450);
+        Stage window = (Stage) ((Node) register.getSource()).getScene().getWindow();
+        window.setScene(RegisterScene);
+        window.show();
     }
 }
