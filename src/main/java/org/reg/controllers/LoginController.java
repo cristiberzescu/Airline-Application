dd02ab2d7d7d81229693ff77962cf97a1f5cd809
cristiberzescu <berzescu.c31@gmail.com>
2022-05-16 01:56:06 +0300

    [AA-30] user structure

--- src/main/java/org/reg/controllers/LoginController.java
+++ src/main/java/org/reg/controllers/LoginController.java
@@ -7,8 +7,14 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import org.reg.exceptions.WrongPasswordException;
+import org.reg.model.User;
+import org.reg.services.UserService;
 
 import java.io.IOException;
+import java.util.Objects;
+
+import static org.reg.services.UserService.getUsers;
 
 public class LoginController {
 
@@ -23,8 +29,39 @@ public class LoginController {
 
     @FXML
     public void handleLoginAction(javafx.event.ActionEvent homepage) throws IOException {
+        try {
+            UserService.loginUser(username.getText(), password.getText());
+            for (User user : getUsers().find()) {
+                if(Objects.equals(username.getText(), user.getUsername())) {
+                    this.loggedUsername = user.getUsername();
+                    this.loggedRole = user.getRole();
+                }
+            }
 
+            if(loggedRole.equals("Customer")) {
+                FXMLLoader Loader = new FXMLLoader();
+                Loader.setLocation(getClass().getClassLoader().getResource("customerPage.fxml"));
+                Parent viewCustomerPage = Loader.load();
+                Scene customerPageScene = new Scene(viewCustomerPage, 650, 450);
+                Stage window = (Stage) ((Node) homepage.getSource()).getScene().getWindow();
+                window.setScene(customerPageScene);
+                window.show();
+            }
+            if (loggedRole.equals("Administrator")) {
+                FXMLLoader Loader = new FXMLLoader();
+                Loader.setLocation(getClass().getClassLoader().getResource("administratorPage.fxml"));
+                Parent viewAdministratorPage = Loader.load();
+                Scene administratorPageScene = new Scene(viewAdministratorPage, 650, 450);
+                Stage window = (Stage) ((Node) homepage.getSource()).getScene().getWindow();
+                window.setScene(administratorPageScene);
+                window.show();
+            }
+
+        } catch (WrongPasswordException e) {
+            password.clear();
+        }
     }
+
     public void goToRegistration(javafx.event.ActionEvent register) throws IOException {
         FXMLLoader Loader = new FXMLLoader();
         Loader.setLocation(getClass().getClassLoader().getResource("register.fxml"));
@@ -34,4 +71,6 @@ public class LoginController {
         window.setScene(RegisterScene);
         window.show();
     }
+
+    public static String getLoggedUsername() { return loggedUsername;}
 }
--- src/main/java/org/reg/services/UserService.java
+++ src/main/java/org/reg/services/UserService.java
@@ -3,8 +3,14 @@ package org.reg.services;
 import org.dizitart.no2.Nitrite;
 import org.dizitart.no2.objects.ObjectRepository;
 import org.reg.model.User;
+import org.reg.exceptions.WrongPasswordException;
 
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.List;
+import java.util.Objects;
+
 
 import static org.reg.services.FileSystemService.getPathToFile;
 
@@ -23,15 +29,46 @@ public class UserService {
 
         userRepository = database.getRepository(User.class);
     }
-
+    public static void loginUser(String username, String password) throws WrongPasswordException {
+        checkPassword(password, username);
+    }
     public static List<User> getAllUsers() {
         return userRepository.find().toList();
     }
 
     public static ObjectRepository<User> getUsers() {return userRepository;}
+    public static void checkPassword(String password, String username) throws WrongPasswordException {
+        int ok = 0;
+        for(User user : userRepository.find()) {
+            if (Objects.equals(username, user.getUsername())) {
+                if (Objects.equals(encodePassword(username, password), user.getPassword())) {
+                    ok = 1;
+                }
+            }
+        }
+        if (ok == 0) {
+            throw new WrongPasswordException();
+        }
+    }
+    public static String encodePassword(String salt, String password) {
+        MessageDigest md = getMessageDigest();
+        md.update(salt.getBytes(StandardCharsets.UTF_8));
 
+        byte[] hashedPassword = md.digest(password.getBytes(StandardCharsets.UTF_8));
 
-
+        // This is the way a password should be encoded when checking the credentials
+        return new String(hashedPassword, StandardCharsets.UTF_8)
+                .replace("\"", ""); //to be able to save in JSON format
+    }
+    private static MessageDigest getMessageDigest() {
+        MessageDigest md;
+        try {
+            md = MessageDigest.getInstance("SHA-512");
+        } catch (NoSuchAlgorithmException e) {
+            throw new IllegalStateException("SHA-512 does not exist!");
+        }
+        return md;
+    }
     public static Nitrite getDatabase() {
         return database;
     }
