7b07dba0fe75d2ee01199942767d19522cc21748
krist <kristyonut@gmail.com>
2022-05-18 23:58:44 +0300

    AA-26: EnrollFlightController , enrollFlightPage.fxml, registerCustomer.fxml updated

--- src/main/java/org/reg/controllers/EnrollFlightController.java
+++ src/main/java/org/reg/controllers/EnrollFlightController.java
@@ -13,11 +13,21 @@ import java.io.IOException;
 public class EnrollFlightController {
     @FXML
     Button backButton;
+    Button enrollButton;
     public void handleBack(javafx.event.ActionEvent login) throws IOException {
         FXMLLoader Loader = new FXMLLoader();
         Loader.setLocation(getClass().getClassLoader().getResource("customerPage.fxml"));
         Parent viewLogin = Loader.load();
         Scene LoginScene = new Scene(viewLogin, 650, 450);
+        Stage window = (Stage) ((Node) login.getSource()).getScene().getWindow();
+        window.setScene(LoginScene);
+        window.show();
+    }
+    public void handleViewEnroll(javafx.event.ActionEvent login) throws IOException {
+        FXMLLoader Loader = new FXMLLoader();
+        Loader.setLocation(getClass().getClassLoader().getResource("viewEnrolledFlightPage.fxml"));
+        Parent viewLogin = Loader.load();
+        Scene LoginScene = new Scene(viewLogin, 650, 520);
         Stage window = (Stage) ((Node) login.getSource()).getScene().getWindow();
         window.setScene(LoginScene);
         window.show();
