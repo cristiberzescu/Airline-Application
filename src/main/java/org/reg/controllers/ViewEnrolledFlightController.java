42457564828c9a3a772007e2a179273d75f66626
krist <kristyonut@gmail.com>
2022-05-19 00:06:39 +0300

    AA-26: viewEnrolledFlightPage.fxml, ViewEnrolledFlightController.java updated

--- src/main/java/org/reg/controllers/ViewEnrolledFlightController.java
+++ src/main/java/org/reg/controllers/ViewEnrolledFlightController.java
@@ -1,4 +1,35 @@
 package org.reg.controllers;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
+import org.reg.model.User;
+import org.reg.services.UserService;
+
+import java.io.IOException;
+
 public class ViewEnrolledFlightController {
+
+    @FXML
+    Button backButton;
+    @FXML
+    TableView flightsTableView;
+
+    public void handleBack(javafx.event.ActionEvent login) throws IOException {
+        FXMLLoader Loader = new FXMLLoader();
+        Loader.setLocation(getClass().getClassLoader().getResource("customerPage.fxml"));
+        Parent viewLogin = Loader.load();
+        Scene LoginScene = new Scene(viewLogin, 650, 450);
+        Stage window = (Stage) ((Node) login.getSource()).getScene().getWindow();
+        window.setScene(LoginScene);
+        window.show();
+    }
+
 }
