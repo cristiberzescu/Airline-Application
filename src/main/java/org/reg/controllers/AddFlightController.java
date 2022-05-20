5d86d6608c7f269b9655eb6ddb07d5038169951c
cristiberzescu <berzescu.c31@gmail.com>
2022-05-20 14:20:24 +0300

    [AA-28] update addFlightPage

--- src/main/java/org/reg/controllers/AddFlightController.java
+++ src/main/java/org/reg/controllers/AddFlightController.java
@@ -1,5 +1,22 @@
 package org.reg.controllers;
 
-public class AddFlightController {
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
+import java.io.IOException;
+public class AddFlightController {
+    @FXML
+    public void handleBack(javafx.event.ActionEvent back) throws IOException {
+        FXMLLoader Loader = new FXMLLoader();
+        Loader.setLocation(getClass().getClassLoader().getResource("administratorPage.fxml"));
+        Parent viewAdmin = Loader.load();
+        Scene AdminScene = new Scene(viewAdmin, 650, 450);
+        Stage window = (Stage) ((Node) back.getSource()).getScene().getWindow();
+        window.setScene(AdminScene);
+        window.show();
+    }
 }
--- src/main/java/org/reg/controllers/AdministratorPageController.java
+++ src/main/java/org/reg/controllers/AdministratorPageController.java
@@ -48,4 +48,5 @@ public class AdministratorPageController {
         stage.show();
     }
 
+
 }
