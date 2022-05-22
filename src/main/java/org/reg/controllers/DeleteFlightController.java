c4a856bccd2eda76c656c9d5a79ca3d300b80d6e
krist <kristyonut@gmail.com>
2022-05-22 22:57:21 +0300

    AA-27: DeleteFlightController.java updated, handleBack method added

--- src/main/java/org/reg/controllers/DeleteFlightController.java
+++ src/main/java/org/reg/controllers/DeleteFlightController.java
@@ -1,4 +1,36 @@
 package org.reg.controllers;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class DeleteFlightController {
+
+
+    @FXML
+    Button backButton;
+    @FXML
+    Button deleteButton;
+    @FXML
+    TableView flightsTableView;
+
+
+    @FXML
+    public void handleBack(javafx.event.ActionEvent adminPage) throws IOException {
+        FXMLLoader Loader = new FXMLLoader();
+        Loader.setLocation(getClass().getClassLoader().getResource("administratorPage.fxml"));
+        Parent viewAdmin = Loader.load();
+        Scene adminScene = new Scene(viewAdmin, 650, 450);
+        Stage window = (Stage) ((Node) adminPage.getSource()).getScene().getWindow();
+        window.setScene(adminScene);
+        window.show();
+    }
 }
+
